153f04271d5d728d287eebca4d41793f6fc14648
Francisco Dans <fdansv@gmail.com>
2015-01-25 19:56:29 +0100

    whole thing working now

--- index.js
+++ index.js
@@ -1,142 +1,104 @@
+#!/usr/bin/env node
+
 var geolib = require("geolib");
 var fs = require('fs');
 
-var testPoly = {
-        "type": "LineString",
-        "coordinates": [
-          [
-            -6.416015625,
-            46.98025235521883
-          ],
-          [
-            -18.017578125,
-            51.83577752045248
-          ],
-          [
-            -19.072265625,
-            58.12431960569377
-          ],
-          [
-            -10.37109375,
-            61.52269494598358
-          ],
-          [
-            3.1640625,
-            62.552856958572896
-          ],
-          [
-            8.349609375,
-            56.511017504952136
-          ],
-          [
-            5.009765625,
-            50.0077390146369
-          ]
-        ]
-      };
+function Polycloud(){
 
-var testPoint = {
-        "type": "Point",
-        "coordinates": [
-          -4.130859375,
-          55.87531083569679
-        ]
-      };
+};
 
-var pointFalse = {
-        "type": "Point",
-        "coordinates": [
-          12.568359375,
-          57.61010702068388
-        ]
-      };
-
-var pointToStupidFormat = function(point){
-	return {latitude: point.coordinates[1], longitude: point.coordinates[0]};
-}
-
-var polygonToStupidFormat = function(polygon){
-	var polArray = [];
-	for (var i = 0; i < polygon.coordinates.length; i++){
-		polArray.push({latitude: polygon.coordinates[i][1], longitude: polygon.coordinates[i][0]})
-	}
-	return polArray;
-}
-
-var pointInPolygon = function(point, polygon){
-	return geolib.isPointInside(point, polygon);
-}
-
-var getGroupedCoordinates = function(polygon){
-	var group = {lats:[], lons:[]};
-	for (var i = 0; i < polygon.length; i++){
-		group.lats.push(polygon[i].latitude);
-		group.lons.push(polygon[i].longitude);
-	}
-	return group;
-}
-
-var getBoundingBox = function(polygon){
-	var grouped = getGroupedCoordinates(polygon);
-	var N = Math.max.apply(null, grouped.lats);
-	var S = Math.min.apply(null, grouped.lats);
-	var E = Math.max.apply(null, grouped.lons);
-	var W = Math.min.apply(null, grouped.lons);
-	return {"N": N, "S": S, "E": E, "W": W};
-}
-
-var backToSaneFormat = function(pointCloud){
-	var geoJSON = {
-	  "type": "FeatureCollection",
-	  "features": []
-	};
-	for (var i = 0; i < pointCloud.length; i++){
-		var geoPoint = {
-	      "type": "Feature",
-	      "properties": {},
-	      "geometry": {
-	        "type": "Point",
-	        "coordinates": [
-	          pointCloud[i].longitude,
-	          pointCloud[i].latitude
-	        ]
-	      }
-	    };
-	    geoJSON.features.push(geoPoint);
-	}
-	return geoJSON;
-}
-
-var saveToFile = function(geojson, _path){
-	var path = _path || "out/polygon.json";
-	fs.writeFile("out/polygon.json", JSON.stringify(geojson), function(err) {
-	    if(err) {
-	        console.log(err);
-	    } else {
-	        console.log("File saved at " + path);
-	    }
-	}); 
-}
-
-var genPointCloud = function(polygon, granularity){
-	var polygon = polygonToStupidFormat(polygon);
-	var boundingBox = getBoundingBox(polygon);
-	var pointCloud = [];
-	var latIncrement = (Math.abs(boundingBox.S - boundingBox.N)) / granularity;
-	var lonIncrement = (Math.abs(boundingBox.W - boundingBox.E)) / granularity;
-	var latPointer = boundingBox.N;
-	while (latPointer >= boundingBox.S){
-		var lonPointer = boundingBox.W;
-		while(lonPointer < boundingBox.E){
-			var pointInStupidFormat = {latitude: latPointer, longitude: lonPointer};
-			if(pointInPolygon(pointInStupidFormat, polygon)){
-				pointCloud.push(pointInStupidFormat);
-			}
-			lonPointer += lonIncrement;
+Polycloud.prototype = {	
+	pointToStupidFormat: function(point){
+		return {latitude: point.coordinates[1], longitude: point.coordinates[0]};
+	},
+	polygonToStupidFormat: function(polygon){
+		var polArray = [];
+		for (var i = 0; i < polygon.coordinates[0].length; i++){
+			polArray.push({latitude: polygon.coordinates[0][i][1], longitude: polygon.coordinates[0][i][0]})
 		}
-		latPointer -= latIncrement;
+		return polArray;
+	},
+	pointInPolygon: function(point, polygon){
+		return geolib.isPointInside(point, polygon);
+	},
+	getGroupedCoordinates: function(polygon){
+		var group = {lats:[], lons:[]};
+		for (var i = 0; i < polygon.length; i++){
+			group.lats.push(polygon[i].latitude);
+			group.lons.push(polygon[i].longitude);
+		}
+		return group;
+	},
+	getBoundingBox: function(polygon){
+		var grouped = this.getGroupedCoordinates(polygon);
+		var N = Math.max.apply(null, grouped.lats);
+		var S = Math.min.apply(null, grouped.lats);
+		var E = Math.max.apply(null, grouped.lons);
+		var W = Math.min.apply(null, grouped.lons);
+		return {"N": N, "S": S, "E": E, "W": W};
+	},
+	backToSaneFormat: function(pointCloud){
+		var geoJSON = {
+		  "type": "FeatureCollection",
+		  "features": []
+		};
+		for (var i = 0; i < pointCloud.length; i++){
+			var geoPoint = {
+		      "type": "Feature",
+		      "properties": {},
+		      "geometry": {
+		        "type": "Point",
+		        "coordinates": [
+		          pointCloud[i].longitude,
+		          pointCloud[i].latitude
+		        ]
+		      }
+		    };
+		    geoJSON.features.push(geoPoint);
+		}
+		return geoJSON;
+	},
+	saveToFile: function(geojson, _path){
+		var path = _path || "out/polygon.json";
+		fs.writeFile(path, JSON.stringify(geojson), function(err) {
+		    if(err) {
+		        console.log(err);
+		    } else {
+		        console.log("File saved at " + path);
+		    }
+		}); 
+	},
+	genPointCloud: function(_polygon, granularity){
+		var polygon = [];
+		if (_polygon.type === "MultiPolygon"){
+			for (var i = 0; i < _polygon.coordinates.length; i++){
+				polygon.push(this.polygonToStupidFormat({coordinates: _polygon.coordinates[i]}));
+			}
+		}
+		else{
+			polygon = [this.polygonToStupidFormat(_polygon)];
+		}
+		var pointCloud = [];
+		for (var i = 0; i < polygon.length; i++){
+			var boundingBox = this.getBoundingBox(polygon[i]);
+			var latIncrement = (Math.abs(boundingBox.S - boundingBox.N)) / granularity;
+			var lonIncrement = (Math.abs(boundingBox.W - boundingBox.E)) / granularity;
+			var latPointer = boundingBox.N;
+			while (latPointer >= boundingBox.S){
+				var lonPointer = boundingBox.W;
+				while(lonPointer < boundingBox.E){
+					var pointInStupidFormat = {latitude: latPointer, longitude: lonPointer};
+					if(this.pointInPolygon(pointInStupidFormat, polygon[i])){
+						pointCloud.push(pointInStupidFormat);
+					}
+					lonPointer += lonIncrement;
+				}
+				latPointer -= latIncrement;
+			}
+		}
+		return this.backToSaneFormat(pointCloud)
 	}
-	saveToFile(backToSaneFormat(pointCloud));
 }
 
-genPointCloud(testPoly, 10);
+module.exports = Polycloud;
+
